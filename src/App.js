1fb8fd1a7064f780f8a1e4bc10f7c1946e7d73e6
usman@carswitch.com <usman@carswitch.com>
2022-08-03 04:38:20 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import List from './List';
+import data from './data';
+
+function App(){
+
+  const [ people, setPeople ] = useState(data);
+
+  return (
+
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthday reminders</h3>
+        <List people={people}/>
+        <button onClick={()=>{ setPeople([]) }}>Clear all</button>
+      </section>
+    </main>
+
+  )
+
+}
+
+export default App;
